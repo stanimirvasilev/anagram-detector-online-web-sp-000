e7fc94cbeacd77f2e55975028b71e0ca20e9419c
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-20 14:51:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
    
   def match(array)
    #take in an array and return all matches in an array, if no matches exist, it should return an empty array.
-  array.scann 
+  array. 
     
   end
   
