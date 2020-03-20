b807fd15e9caccef8cffcfb5514018a46a7076bb
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-20 13:30:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,13 +2,16 @@
 
 class Anagram
   
+  attr_accessor :word
+  
+  
   def initialization(word)
     @word = word
   end
    
-  def match
+  def match(array)
    #take in an array and return all matches in an array, if no matches exist, it should return an empty array.
-  
+  array.scann 
     
   end
   
